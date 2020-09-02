f35b9ffbcd037e5ffd7451179a89109958198b13
Marek Wiśniowski <mwisniowski@o2.pl>
2020-09-02 12:26:47 +0200

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,12 +10,13 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
     render() {
-        const isQuizEnd = quizData.quiz_questions[this.state.quiz_position - 1] === quizData.quiz_questions.length
+        const isQuizEnd = ((this.state.quiz_position - 1) === 
+        quizData.quiz_questions.length)
         return (
             <div>
-                <QuizEnd isQuizEnd={true} />
+                {isQuizEnd ? <QuizEnd /> :
                 <QuizQuestion quiz_question={quizData.quiz_questions
-                [this.state.quiz_position - 1]} />
+                [this.state.quiz_position - 1]} />}
             </div>
         )
     }
